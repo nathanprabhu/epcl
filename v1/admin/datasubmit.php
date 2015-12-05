9d0a1a1e16b3ff4aa20d197a6497ac2ca6d29f62
nathanprabhu <prabhunathan.g@gmail.com>
2015-12-05 01:44:17 -0600

    Beta

--- v1/admin/datasubmit.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo ("<SCRIPT LANGUAGE='JavaScript'>
-        window.alert('The data is sent for review. Updations will be made after Successful review')
-        window.location.href='dashboard.php'
-        </SCRIPT>");
-?>
